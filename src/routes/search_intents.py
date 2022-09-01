3f951c454c7ec48b99f2d2707694abba6b165610
smadnaik <sandeep.madnaik@springml.com>
2022-09-01 16:17:39 +0530

    Changed request to json. Updated reponse messages.

--- src/routes/search_intents.py
+++ src/routes/search_intents.py
@@ -8,6 +8,7 @@ from flask import request, jsonify
 from src import app
 from src.helpers import constant
 from src.helpers.gCloud import firestore_helper as fsh
+from flask_api import status
 
 ROUTE = "/detectIntent"
 
@@ -20,17 +21,13 @@ def detect_intent():
     project_id = os.getenv(constant.PROJECT_ID, "retail-btl-dev")
     location_id = "global"
 
-    if constant.CUSTOMER_ID not in request.form.keys():
-        resp = jsonify({constant.MESSAGE: constant.CUSTOMER_MANDATORY})
-        resp.status_code = 400
-        return resp
-    customer_id = request.form[constant.CUSTOMER_ID]
-    agent_id = fsh.get_agent_id_by_customer_id(customer_id)
-
-    agent = f"projects/{project_id}/locations/{location_id}/agents/{agent_id}"
-    data = request.form
-    # print(data['text_input'])
+    data = request.json
+    customer_id = data[constant.CUSTOMER_ID]
     text_input = data["text_input"]
+
+    agent_id = fsh.get_agent_id_by_customer_id(customer_id)
+    agent = f"projects/{project_id}/locations/{location_id}/agents/{agent_id}"
+
     try:
         text_input = text_input[:255]  # dialog
         DIALOGFLOW_LANGUAGE_CODE = "en"
@@ -41,19 +38,19 @@ def detect_intent():
         query_input = df.QueryInput(
             text=text_input, language_code=DIALOGFLOW_LANGUAGE_CODE
         )
-        detectIntent = df.DetectIntentRequest(
+        detect_intent_request = df.DetectIntentRequest(
             session=session_path, query_input=query_input
         )
-        response = session_client.detect_intent(request=detectIntent)
+        response = session_client.detect_intent(request=detect_intent_request)
         # print(response.query_result.intent.display_name)
         # print(response.query_result.response_messages)
         response_messages = [
             " ".join(msg.text.text) for msg in response.query_result.response_messages
         ]
         print(f"Response text: {' '.join(response_messages)}\n")
-        return "".join(response_messages)
-    except:
+        return {"success": True, "fulfillments": response_messages}, status.HTTP_200_OK
+    except Exception:
         print("exception while input")
         print(text_input)
         print("Unexpected error:", sys.exc_info()[0])
-        return "Error"
+        return {"success": False, "message": "Error while getting fulfillments."}, status.HTTP_400_BAD_REQUEST
