ae7e7ae0a65882b60adef4042a7258a315bae1a6
harinathareddybollapuspringml <98870480+harinathareddybollapuspringml@users.noreply.github.com>
2022-09-01 14:18:40 +0530

    Dialog flow backend changes are merged

--- src/routes/search_intents.py
+++ src/routes/search_intents.py
@@ -11,12 +11,13 @@ from src.helpers.gCloud import firestore_helper as fsh
 
 ROUTE = "/detectIntent"
 
+
 @app.route(ROUTE, methods=["GET"])
 def detect_intent():
-    '''
-    Retuns the fulfillment text corresponding the intent
-    '''
-    project_id = constant.PROJECT_ID
+    """
+    Returns the fulfillment text corresponding the intent
+    """
+    project_id = os.getenv(constant.PROJECT_ID, "retail-btl-dev")
     location_id = "global"
 
     if constant.CUSTOMER_ID not in request.form.keys():
@@ -28,25 +29,29 @@ def detect_intent():
 
     agent = f"projects/{project_id}/locations/{location_id}/agents/{agent_id}"
     data = request.form
-    #print(data['text_input'])
-    text_input = data['text_input']
+    # print(data['text_input'])
+    text_input = data["text_input"]
     try:
-        text_input = text_input[:255] #dialog
+        text_input = text_input[:255]  # dialog
         DIALOGFLOW_LANGUAGE_CODE = "en"
         SESSION_ID = uuid.uuid4()
         session_path = f"{agent}/sessions/{SESSION_ID}"
         session_client = df.SessionsClient()
         text_input = df.TextInput(text=text_input)
-        query_input = df.QueryInput(text=text_input, language_code=DIALOGFLOW_LANGUAGE_CODE)
-        detectIntent = df.DetectIntentRequest(session=session_path,query_input=query_input)
+        query_input = df.QueryInput(
+            text=text_input, language_code=DIALOGFLOW_LANGUAGE_CODE
+        )
+        detectIntent = df.DetectIntentRequest(
+            session=session_path, query_input=query_input
+        )
         response = session_client.detect_intent(request=detectIntent)
-        #print(response.query_result.intent.display_name)
-        #print(response.query_result.response_messages)
+        # print(response.query_result.intent.display_name)
+        # print(response.query_result.response_messages)
         response_messages = [
-        " ".join(msg.text.text) for msg in response.query_result.response_messages
+            " ".join(msg.text.text) for msg in response.query_result.response_messages
         ]
         print(f"Response text: {' '.join(response_messages)}\n")
-        return ''.join(response_messages)
+        return "".join(response_messages)
     except:
         print("exception while input")
         print(text_input)
