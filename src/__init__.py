98e7f11a0b056d5eae9837bcb8a8ebc567fa371f
harinathareddybollapuspringml <98870480+harinathareddybollapuspringml@users.noreply.github.com>
2022-09-02 10:20:40 +0530

    Swagger configuration updated

--- src/__init__.py
+++ src/__init__.py
@@ -1,11 +1,29 @@
 """
 All Routes
 """
+import os
 from flask import Flask
 from flask_cors import CORS
+from flasgger import Swagger
 from config import envConfig, gCloudConfig
 
+
 app = Flask(__name__)
+
+# Swagger configurations
+swagger_config = {
+    "headers": [],
+    "specs": [{"endpoint": "documentacao_api", "route": "/documentacao_api.json"}],
+    "static_url_path": "/flasgger_static",
+    "specs_route": "/api/docs/",
+}
+
+app.config["SWAGGER"] = {
+    "title": "Beyond The Label - APIs",
+    # 'uiversion': 2
+}
+swagger = Swagger(app, config=swagger_config, template_file='swagger.yml')
+# end of swagger configuration
 
 CORS(app)
 
