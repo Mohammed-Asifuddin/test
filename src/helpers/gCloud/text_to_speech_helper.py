e17c6207efd4dd362e12336be7c5028b4c7a3da5
harinathareddybollapuspringml <98870480+harinathareddybollapuspringml@users.noreply.github.com>
2022-09-29 11:33:12 +0530

    Update text_to_speech_helper.py

--- src/helpers/gCloud/text_to_speech_helper.py
+++ src/helpers/gCloud/text_to_speech_helper.py
@@ -20,9 +20,4 @@ def convert_text_to_speech(text):
     response = client.synthesize_speech(
         input=synthesis_input, voice=voice, audio_config=audio_config
     )
-    # The response's audio_content is binary.
-    with open("output.mp3", "wb") as out:
-        # Write the response to the output file.
-        out.write(response.audio_content)
-        print('Audio content written to file "output.mp3"')
     return response.audio_content
